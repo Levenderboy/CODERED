1c19ddce2ba82d6ae24ba39701e03594aba437c0
Levenderboy <141110863+Levenderboy@users.noreply.github.com>
2024-05-12 14:30:15 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "33"    // 33
+score = "33abc"     // NaN
+score = null        // 0
+score = true        // 1 // false => 0
+score = undefined   // NaN
+score = "shivam"    // NaN
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valInNumber = Number(score)
+console.log(typeof(valInNumber))
+console.log(valInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// Empty String "" => false 
+// Full String "Shivam" => true 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
