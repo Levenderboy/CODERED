32ebe67758e1d31312730e5e00a6e70c45bba362
Levenderboy <141110863+Levenderboy@users.noreply.github.com>
2024-05-13 14:24:48 +0000

    Operations in JavaScript

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,4 @@
+/*
 let score = "33"    // 33
 score = "33abc"     // NaN
 score = null        // 0
@@ -24,3 +25,35 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof(stringNumber))
+
+*/
+// ************* OPERATIONS **************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Shivam"
+console.log(str3 = str1 + str2);
+console.log(typeof(str3));
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 1000
+a = gameCounter++;
+b = ++gameCounter
+console.log(a, b);      //100 102
+
+// ASSIGNMENT
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
